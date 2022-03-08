a77f75d92c6793e98919d46d2a26dc5a229cdf5e
Yash Kaul <kauly.cs@gmail.com>
2022-03-07 19:52:24 -0800

    updated wgraphs with input testing

--- main.cpp
+++ main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <list>
 #include "wgraph.h"
 
+void ReadFile(std::string file, Wgraph& w) {
+
+    std::ifstream input(file);
+    if (!input.good()) {
+        std::cerr << "ERROR: failed to open input file" << std::endl;
+        exit(1);
+    }
+    std::string prev = "false";
+    std::string buffer;
+    while(input >> buffer) {
+        std::string buffer2;
+        input >> buffer2;
+        w.add(buffer,buffer2,100);
+        if (prev != "false") 
+            w.connect(buffer,prev);
+        prev = buffer;
+    }
+
+    input.close();
+}
+
 int main(int argc, char * argv[]) {
 
+    if (argc != 2) {
+        std::cerr << "ERROR: wrong number of inputs detected." << std::endl;
+        exit(1);
+    }
+    // argv[2] == input file for testing purposes.
     Wgraph test1 = Wgraph();
-    test1.add("a",100);
-    test1.add("b",100);
-    test1.add("c",100);
+    ReadFile(std::string(argv[1]), test1);
     test1.print();
-    test1.connect("a","b");
-    test1.connect("a","c");
     test1.printConnect();
 
 
--- wgraph.h
+++ wgraph.h
@@ -1,25 +1,27 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 #include <list>
 #include <map>
 #include <vector>
 
 class Node {
     public:
-        Node() : tag(""), size(0) {}
-        Node(std::string t, int s) : tag(t), size(s) {}
+        Node() : tag(""), link(""), size(0) {}
+        Node(std::string t, std::string l, int s) : tag(t), link(l), size(s) {}
 
         void add(Node * n) { adj.push_back(n); }
 
         friend std::ostream& operator<<(std::ostream& ostr, Node* n);
 
         std::string tag;
+        std::string link;
         int size;
         std::list<Node*> adj;
 };
 std::ostream& operator<<(std::ostream& ostr, Node* n) {
-    ostr << "(" << n->tag << "," << n->size << ")";
+    ostr << "(" << n->tag << "," <<  n->link << "," << n->size << ")";
     return ostr;
 }
 
@@ -27,7 +29,7 @@ class Wgraph {
     public:
         Wgraph() : N(0) {}
         int size() const { return N; }
-        void add(std::string t, int s) { table.insert(make_pair(t,new Node(t,s))); }
+        void add(std::string t, std::string l, int s) { table.insert(make_pair(t,new Node(t,l,s))); }
         void connect(std::string t1, std::string t2) { find(t1)->add(find(t2)); find(t2)->add(find(t1)); }
         Node * find(std::string t) {
             if(table.find(t) != table.end()) return table.find(t)->second;
@@ -35,7 +37,7 @@ class Wgraph {
         void printConnect() { 
             int i = 0;
             for (std::map<std::string,Node*>::iterator itr = table.begin(); itr != table.end(); itr++, i++) {
-                std::cout << i << ": " <<  itr->first << " : ";
+                std::cout << std::setw(2) << i << ": " <<  itr->first << " : ";
                 for (std::list<Node*>::iterator itr2 = itr->second->adj.begin(); itr2 != itr->second->adj.end(); itr2++)
                     std::cout << " " << *itr2;
                 std::cout << std::endl;
@@ -44,7 +46,7 @@ class Wgraph {
         void print() {
             int i = 0;
             for (std::map<std::string,Node*>::iterator itr = table.begin(); itr != table.end(); itr++, i++)
-                std::cout << i << ": " <<  itr->first << std::endl;
+                std::cout << std::setw(2) << i << ": " <<  itr->first << std::endl;
         }
 
 
