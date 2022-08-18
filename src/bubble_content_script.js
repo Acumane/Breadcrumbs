f6a33353e30cc248488fd99515eeb4c6c12e1b31
Yash Kaul <kauly.cs@gmail.com>
2022-08-17 22:00:38 -0400

    graph documentation

--- src/bubble_content_script.js
+++ src/bubble_content_script.js
@@ -7,13 +7,13 @@ chrome.storage.local.get("bubble_bool",function(result) {
 
 window.addEventListener("keydown",function(e){
     if (e.key === "b") {
-        if (startBubble) startBubble = false;
-        else startBubble = true;
-        if (startBubble) {
-            console.log(window.location.href);
-        }
+
+        chrome.windows.create({url: "https://www.google.com/", type: "popup"});
+
         chrome.storage.local.set({"bubble_bool":startBubble},function(){});
     }  
 });
+
+console.log(window.location.href);
 
 
--- src/graph.js
+++ src/graph.js
@@ -1,3 +1,13 @@
+/* The graph is fed data about several parameters before constructing the graoh:
+width -- width of browser screen
+height -- height of browser screen
+radius -- size of graph bubbles
+renderer -- custom D3 object that renders the graph object
+stage -- stage for the graph to be rendered on
+lines -- individual lines to be rendered between nodes
+circles -- nodes to be drawn onto the graph
+color -- special style option for inner circle color
+*/
 const width    = viewportWidth,
       height   = viewportHeight,
       radius   = 4,
@@ -6,9 +16,11 @@ const width    = viewportWidth,
       lines    = new PIXI.Graphics(),
       circles  = new PIXI.Container(),
       color    = d3.scaleOrdinal(d3.schemeCategory10);
+// resize checks for the current window size in case of resize'd browser 
 renderer.resize(window.innerWidth-100, window.innerHeight);
-// spawn location
+// spawn location should be in central location of screen
 let transform = d3.zoomIdentity.translate(width / 2, height / 2);
+// simulation is the entry for the d3 renderer.
 const simulation = d3.forceSimulation()
   .force('charge', d3.forceManyBody())
   .force('link',   d3.forceLink().id(d => d.id))
@@ -17,16 +29,22 @@ const simulation = d3.forceSimulation()
 
 const main = () => d3.json('data.json', (error, graph) => {
   if (error) throw error;
+
+  // appends child to newTab main list of objects
   document.body.appendChild(renderer.view);
   
+  // renderer stylized options to fit with mainline CSS
   renderer.backgroundColor = 0x202023;
   renderer.backgroundAlpha = 0;
+  // add nodes and lines to stage
   stage.addChild(lines);
   stage.addChild(circles);
 
+  // for every node add options
   for (const node of graph.nodes) {
     const circle = new PIXI.Graphics();
 
+    // draw basic nodes with edges interlinking
     circle.beginFill(Number.parseInt(color(node.user).substr(1), 16));
     circle.lineStyle(1.5, 0xffffff);
     circle.drawCircle(0, 0, radius);
@@ -37,6 +55,8 @@ const main = () => d3.json('data.json', (error, graph) => {
   }
 
   const zoomed = () => {
+    // transform the event with stage parameters that
+    // were assigned previously
     transform = d3.event.transform;
 
     stage.x       = transform.x;
@@ -44,33 +64,36 @@ const main = () => d3.json('data.json', (error, graph) => {
     stage.scale.x = transform.k;
     stage.scale.y = transform.k;
 
+    // request the renderer to apply the stage to the document
     requestAnimationFrame(() => renderer.render(stage));
   };
 
+  // return simulation object given node size and positions when dragging
   const dragsubject = () => {
     return simulation
       .find(transform.invertX(d3.event.x), transform.invertY(d3.event.y), radius);
   };
 
+  // when drag started add latency to x/y positions 
   const dragstarted = () => {
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
 
     d3.event.subject.fx = d3.event.subject.x;
     d3.event.subject.fy = d3.event.subject.y;
   };
-
+  // dragged status applies transform to buffer position change
   const dragged = () => {
     d3.event.subject.fx += d3.event.dx / transform.k;
     d3.event.subject.fy += d3.event.dy / transform.k;
   };
-
+  // end conditions for drag which release momentum from node
   const dragended = () => {
     if (!d3.event.active) simulation.alphaTarget(0);
 
     d3.event.subject.fx = null;
     d3.event.subject.fy = null;
   };
-
+  // readraw function that will re-link edges to proper nodes and re-render stage properly
   const redraw = () => requestAnimationFrame(() => {
     lines.clear();
     lines.lineStyle(1, 0x999999, 0.6);
@@ -87,9 +110,10 @@ const main = () => d3.json('data.json', (error, graph) => {
     .on('tick.redraw', redraw)
     .nodes(circles.children)
     .force('link').links(graph.links);
-
+  // zoom object is some scalar factor on screen.
   const zoom = d3.zoom();
-
+  // select the renderer to view the object and transform by
+  // the appropriate zoom
   d3.select(renderer.view)
     .call(d3.drag()
       .subject(dragsubject)
@@ -99,5 +123,5 @@ const main = () => d3.json('data.json', (error, graph) => {
     .call(zoom.on('zoom', zoomed))
     .call(zoom.transform, transform);
 });
-
+// add event listener for D3 functionality to main page
 document.addEventListener('DOMContentLoaded', main);
